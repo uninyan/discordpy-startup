9016bbf64b9bd8f9493649e00cb70ccbbb30b918
uninyan <74971984+uninyan@users.noreply.github.com>
2020-11-24 23:33:51 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -12,13 +12,22 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
+# 発言時に実行されるイベントハンドラを定義
+@bot.event
+async def on_message(message):
+    if bot.user in message.mentions: # 話しかけられたかの判定
+        await reply(message) # 返信する非同期関数を実行
 
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
 
-@bot.command()
-async def uni(ctx):
-    await ctx.send('uni!!!')
+# 返信する非同期関数を定義
+async def reply(message):
+    reply = f'{message.author.mention} 呼んだ？' # 返信メッセージの作成
+    await message.channel.send(reply) # 返信メッセージを送信
+
+
+
 
 bot.run(token)
